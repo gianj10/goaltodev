e85fdcdb31856bba0fa8d4d5f024a9d0c76f33da
Gian Joy Herrera <herreragianjoy@gmail.com>
2023-12-11 17:10:03 +0800

    Add the swiper library

--- resources/js/index.js
+++ resources/js/index.js
@@ -4,3 +4,9 @@ import Swiper from 'swiper';
 import 'swiper/css';
 
 const swiper = new Swiper();
+
+
+// import function to register Swiper custom elements
+import { register } from 'swiper/element/bundle';
+// register Swiper custom elements
+register();
