e3dd48f2334641e367906e768e1c38df8de9e68c
Gian Joy Herrera <herreragianjoy@gmail.com>
2023-12-13 17:31:28 +0800

    Changes using the swiperslide

--- resources/js/index.js
+++ resources/js/index.js
@@ -2,11 +2,3 @@
 import Swiper from 'swiper';
 // import Swiper styles
 import 'swiper/css';
-
-const swiper = new Swiper();
-
-
-// import function to register Swiper custom elements
-import { register } from 'swiper/element/bundle';
-// register Swiper custom elements
-register();
